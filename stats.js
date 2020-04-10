75c610c402f6d28a46b60928d25e3256466538a7
Marcus Paulo <mlima.paulo9@gmail.com>
2020-04-10 09:59:50 -0300

    momitor de memoria

---
+++ stats.js
@@ -0,0 +1,28 @@
+const os = require('os')//importando modulo do node
+
+setInterval(()=>{
+
+    console.log(os.platform())//mostrar no terminal
+
+    const {freemem, totalmem} = os //Estratégia de desconstruir o objeto
+    
+    console.log(`${parseInt(freemem() /1024 /1024)} MB` ,parseInt(totalmem() /1024 /1024))
+    
+    const total = parseInt(totalmem() /1024 /1024)
+    const mem = parseInt(freemem() /1024 /1024)
+    const percents = parseInt((mem/total)*100)
+    
+    console.log(mem, total, percents)
+    
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    
+    }
+    
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.table(stats)
+
+},5000)
